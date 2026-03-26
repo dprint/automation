52aae8df79f77a1fc7ab7bff488c0ba667f6f762
David Sherret <dsherret@gmail.com>
2026-03-26 09:36:16 -0400

    fix: export tasks

--- mod.ts
+++ mod.ts
@@ -3,4 +3,4 @@ export * as dax from "dax";
 export * as semver from "semver";
 export * from "./cargo.ts";
 export * from "./hash.ts";
-export * as processPlugin from "./process_plugin.ts";
+export * as processPlugin from "./process-plugin.ts";
