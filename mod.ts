c93e66f7e1eab7122ab63a3d68713e28aadebb1c
David Sherret <dsherret@gmail.com>
2026-03-26 10:17:28 -0400

    fix: export changelog file

--- mod.ts
+++ mod.ts
@@ -2,5 +2,6 @@ export { default as $ } from "dax";
 export * as dax from "dax";
 export * as semver from "semver";
 export * from "./cargo.ts";
+export * from "./changelog.ts";
 export * from "./hash.ts";
 export * as processPlugin from "./process-plugin.ts";
